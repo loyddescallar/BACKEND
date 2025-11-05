53e045c8a213721775d33469ab31968d85982eec
loyddescallar <loyddescallar@gmail.coom>
2025-11-05 12:07:19 +0800

    CRUD ON BOOK TABLE

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -12,3 +12,40 @@ export const fetchBooks = async (req, res) =>{
         })
     }
 }
+export const createBook = async (req, res) => {
+  const { name, genre, status } = req.body;
+  try {
+    const id = await insertBook(name, genre, status);
+    res.json({ id });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Failed to create book' });
+  }
+};
+
+export const editBook = async (req, res) => {
+    const { title, genre, status } = req.body;
+    const { id } = req.params;
+
+    try {
+        const updatedId = await BookModel.updateBook(id, title, genre, status);
+        res.status(200).json({ success: true, message:updatedId });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({success: false, message: 'Internal Server Error'});
+
+    }
+};
+
+
+export const deleteBook = async (req, res) => {
+  const { d } = req.params;
+
+    try {
+        const deletedId = await BookModel.deleteeBook(id);
+        res.status(200).json({ success: true, deletedId });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({success: false, message: 'Internal Server Error'});
+    }   
+};
--- models/BookModel.js
+++ models/BookModel.js
@@ -1,6 +1,30 @@
 import pool from './db.js';
 
-export const getBooks = async () =>{
-    const [rows] = await pool.query("SELECT * FROM tbl_book")
-    return rows;
+export const getBooks = async () => {
+  const [rows] = await pool.query("SELECT * FROM tbl_book");
+  return rows;
+};
+
+export const insertBook = async (name, genre, status) => {
+  const [result] = await pool.query(
+    'INSERT INTO tbl_book (name, genre, status) VALUES (?, ?, ?)',
+    [name, genre, status]
+  );
+  return result.insertId;
+};
+
+export const updateBook = async (name, genre, status) => {
+    const [result] = await pool.query(
+        "UPDATE tbl_book SET name= ?, genre= ?, status= ? WHERE id= ?",
+        [title, genre, status, bookId]
+    );
+    return result.affectedRows;
 }
+
+export const deleteBook = async (id) => {
+    const [result] = await pool.query(
+        'DELETE FROM tbl_book WHERE id = ?',
+        [id]
+    );
+    return result.affectedRows;
+};
